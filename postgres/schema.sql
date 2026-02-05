ecb2b4731b6c2f26bd649e49e01d193a643c94a9
Danijel Vukoje <danijel.vukoje@gmail.com>
2026-02-05 20:15:58 +0200

    pg schema migration for settings

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -2,12 +2,13 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 16.4 (Debian 16.4-1.pgdg120+2)
--- Dumped by pg_dump version 16.4 (Debian 16.4-1.pgdg120+2)
+-- Dumped from database version 17.7 (Debian 17.7-3.pgdg12+1)
+-- Dumped by pg_dump version 17.7 (Debian 17.7-3.pgdg12+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -16,9 +17,91 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- Name: event_type; Type: TYPE; Schema: public; Owner: -
+--
+
+CREATE TYPE public.event_type AS ENUM (
+    'update',
+    'delete'
+);
+
+
+--
+-- Name: resource_kind; Type: TYPE; Schema: public; Owner: -
+--
+
+CREATE TYPE public.resource_kind AS ENUM (
+    'document',
+    'property'
+);
+
+
+--
+-- Name: user_kind; Type: TYPE; Schema: public; Owner: -
+--
+
+CREATE TYPE public.user_kind AS ENUM (
+    'user',
+    'unit',
+    'org'
+);
+
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: document; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.document (
+    owner text NOT NULL,
+    application text NOT NULL,
+    type text NOT NULL,
+    key text NOT NULL,
+    version bigint DEFAULT 1 NOT NULL,
+    schema_version text NOT NULL,
+    title text NOT NULL,
+    created timestamp with time zone DEFAULT now() NOT NULL,
+    updated timestamp with time zone DEFAULT now() NOT NULL,
+    updated_by text NOT NULL,
+    payload jsonb NOT NULL
+);
+
+
+--
+-- Name: eventlog; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.eventlog (
+    id bigint NOT NULL,
+    owner text NOT NULL,
+    created timestamp with time zone DEFAULT now() NOT NULL,
+    type public.event_type NOT NULL,
+    resource_kind public.resource_kind NOT NULL,
+    application text NOT NULL,
+    document_type text,
+    updated_by text NOT NULL,
+    key text NOT NULL,
+    payload jsonb
+);
+
+
+--
+-- Name: eventlog_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+ALTER TABLE public.eventlog ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.eventlog_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
 
 --
 -- Name: inbox_message; Type: TABLE; Schema: public; Owner: -
@@ -75,6 +158,20 @@ CREATE TABLE public.message_write_lock (
 
 
 --
+-- Name: property; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.property (
+    owner text NOT NULL,
+    application text NOT NULL,
+    key text NOT NULL,
+    value text NOT NULL,
+    created timestamp with time zone DEFAULT now() NOT NULL,
+    updated timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+--
 -- Name: schema_version; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -89,8 +186,25 @@ CREATE TABLE public.schema_version (
 
 CREATE TABLE public."user" (
     sub text NOT NULL,
-    created timestamp with time zone DEFAULT now() NOT NULL
+    created timestamp with time zone DEFAULT now() NOT NULL,
+    kind public.user_kind DEFAULT 'user'::public.user_kind NOT NULL
 );
+
+
+--
+-- Name: document document_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.document
+    ADD CONSTRAINT document_pkey PRIMARY KEY (owner, application, type, key);
+
+
+--
+-- Name: eventlog eventlog_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.eventlog
+    ADD CONSTRAINT eventlog_pkey PRIMARY KEY (id);
 
 
 --
@@ -126,11 +240,34 @@ ALTER TABLE ONLY public.message_write_lock
 
 
 --
+-- Name: property property_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.property
+    ADD CONSTRAINT property_pkey PRIMARY KEY (owner, application, key);
+
+
+--
 -- Name: user user_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public."user"
     ADD CONSTRAINT user_pkey PRIMARY KEY (sub);
+
+
+--
+-- Name: eventlog_owner_id_idx; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX eventlog_owner_id_idx ON public.eventlog USING btree (owner, id);
+
+
+--
+-- Name: document document_owner_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.document
+    ADD CONSTRAINT document_owner_fkey FOREIGN KEY (owner) REFERENCES public."user"(sub) ON DELETE CASCADE;
 
 
 --
@@ -155,6 +292,14 @@ ALTER TABLE ONLY public.message
 
 ALTER TABLE ONLY public.message_write_lock
     ADD CONSTRAINT message_write_lock_recipient_fkey FOREIGN KEY (recipient) REFERENCES public."user"(sub) ON DELETE CASCADE;
+
+
+--
+-- Name: property property_owner_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.property
+    ADD CONSTRAINT property_owner_fkey FOREIGN KEY (owner) REFERENCES public."user"(sub) ON DELETE CASCADE;
 
 
 --
---
+++ schema/002_settings.sql
@@ -0,0 +1,70 @@
+CREATE TYPE user_kind AS ENUM ('user', 'unit', 'org');
+
+ALTER TABLE "user" ADD COLUMN kind user_kind not null default 'user';
+
+CREATE TABLE IF NOT EXISTS document (
+  owner text not null,
+  application text NOT NULL,
+  type text NOT NULL,
+  key text NOT NULL,
+  version bigint not null default 1,
+  schema_version text not null,
+  title text not null,
+  created timestamptz not null default now(),
+  updated timestamptz not null default now(),
+  updated_by text not null,
+  payload jsonb NOT NULL,
+  primary key (owner, application, type, key),
+  foreign key(owner) references "user"(sub)
+    on delete cascade
+);
+
+CREATE TABLE IF NOT EXISTS property (
+  owner text not null,
+  application text NOT NULL,
+  key text NOT NULL,
+  value text NOT NULL,
+  created timestamptz not null default now(),
+  updated timestamptz not null default now(),
+  primary key (owner, application, key),
+  foreign key(owner) references "user"(sub)
+    on delete cascade
+);
+
+CREATE TYPE event_type AS ENUM ('update', 'delete');
+CREATE TYPE resource_kind AS ENUM ('document', 'property');
+
+CREATE TABLE IF NOT EXISTS eventlog (
+  id bigint generated always as identity primary key,
+  owner text not null,
+  created timestamptz not null default now(),
+  -- enum: 'update', 'delete'
+  type event_type NOT NULL, 
+  -- enum: 'document', 'property'
+  resource_kind resource_kind NOT NULL,
+  -- resource identifiers
+  application text NOT NULL,
+  document_type text, -- empty if resource_kind is 'property'
+  updated_by text not null,
+  key text not null,
+  -- read_only we calculate on fly
+  -- Property obj, DocumentMetadata obj, null or empty for deletes
+  payload jsonb
+);
+
+-- give me events for owner X (user, matching unit and org) after id Y
+-- owner in array operation for query
+CREATE INDEX IF NOT EXISTS eventlog_owner_id_idx ON eventlog (owner, id);
+
+---- create above / drop below ----
+
+DROP TABLE IF EXISTS eventlog;
+DROP TABLE IF EXISTS property;
+DROP TABLE IF EXISTS document;
+
+DROP TYPE IF EXISTS resource_kind;
+DROP TYPE IF EXISTS event_type;
+
+ALTER TABLE "user" DROP COLUMN IF EXISTS kind;
+
+DROP TYPE IF EXISTS user_kind;
