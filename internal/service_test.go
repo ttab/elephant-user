3cebcaa414dd554cbe6c26fc0d4dd10596e046f4
Danijel Vukoje <danijel.vukoje@gmail.com>
2026-02-12 18:23:04 +0200

    integration tests for settings service

--- internal/service_test.go
+++ internal/service_test.go
@@ -31,22 +31,26 @@ func TestService(t *testing.T) {
 
 	testData := filepath.Join("..", "testdata")
 
-	eu := startElepantUser(t)
+	eu := startElephantUser(t)
 
 	subjectClaim := "tester"
 	recipient := "core://user/" + subjectClaim
+	orgTest := "core://org/test"
 
-	token := eu.AccessToken(t, elephantine.JWTClaims{
+	userToken := eu.AccessToken(t, elephantine.JWTClaims{
 		Scope: "user",
 		RegisteredClaims: jwt.RegisteredClaims{
 			Issuer:  "test",
 			Subject: subjectClaim,
 		},
+		Org:   orgTest,
+		Units: []string{"core://unit/test"},
 	})
 
 	ctx := t.Context()
+
 	authCtx, _ := twirp.WithHTTPRequestHeaders(ctx, http.Header{
-		"Authorization": []string{"Bearer " + token},
+		"Authorization": []string{"Bearer " + userToken},
 	})
 
 	wg := sync.WaitGroup{}
@@ -163,12 +167,247 @@ func TestService(t *testing.T) {
 	})
 	test.Must(t, err, "push message")
 
+	// Settings
+
+	docApp := "se.ecms.local.test.assignments"
+	docType := "core/view-setting"
+	docKey := "current"
+
+	_, err = eu.Settings.UpdateDocument(authCtx, &user.UpdateDocumentRequest{
+		Application:   docApp,
+		Type:          docType,
+		Key:           docKey,
+		SchemaVersion: "v1.0.0",
+		Payload: &newsdoc.Document{
+			Type:  docType,
+			Title: "Assignments",
+			Meta: []*newsdoc.Block{
+				{
+					Type:        docType,
+					Contenttype: "assignment-type",
+					Role:        "filter",
+					Value:       "picture",
+				},
+				{
+					Type:  docType,
+					Role:  "sortorder",
+					Value: "created",
+					Data: map[string]string{
+						"order": "desc",
+					},
+				},
+			},
+		},
+	})
+	test.Must(t, err, "update document")
+
+	getDocument1, err := eu.Settings.GetDocument(authCtx, &user.GetDocumentRequest{
+		Application: docApp,
+		Type:        docType,
+		Key:         docKey,
+	})
+	test.Must(t, err, "get document")
+
+	test.TestMessageAgainstGolden(t, regenerate, getDocument1,
+		filepath.Join(testData, "get_document_1.json"),
+		test.IgnoreTimestamps{})
+
+	listDocuments1, err := eu.Settings.ListDocuments(authCtx, &user.ListDocumentsRequest{
+		Application: docApp,
+	})
+	test.Must(t, err, "list documents")
+
+	test.TestMessageAgainstGolden(t, regenerate, listDocuments1,
+		filepath.Join(testData, "list_documents_1.json"),
+		test.IgnoreTimestamps{})
+
+	// Properties
+
+	propApp := "se.ecms.local.test.preferences"
+
+	_, err = eu.Settings.SetProperties(authCtx, &user.SetPropertiesRequest{
+		Properties: []*user.PropertyUpdate{
+			{Application: propApp, Key: "prop1", Value: "val1"},
+			{Application: propApp, Key: "prop2", Value: "val2"},
+		},
+	})
+	test.Must(t, err, "set properties")
+
+	getProperties1, err := eu.Settings.GetProperties(authCtx, &user.GetPropertiesRequest{
+		Application: propApp,
+		Keys:        []string{"prop1", "prop2"},
+	})
+	test.Must(t, err, "get properties")
+
+	test.TestMessageAgainstGolden(t, regenerate, getProperties1,
+		filepath.Join(testData, "get_properties_1.json"),
+		test.IgnoreTimestamps{})
+
+	_, err = eu.Settings.DeleteProperties(authCtx, &user.DeletePropertiesRequest{
+		Properties: []*user.PropertyDelete{
+			{Application: propApp, Key: "prop1"},
+		},
+	})
+	test.Must(t, err, "delete properties")
+
+	getProperties2, err := eu.Settings.GetProperties(authCtx, &user.GetPropertiesRequest{
+		Application: propApp,
+		Keys:        []string{"prop1", "prop2"},
+	})
+	test.Must(t, err, "get properties after deletion")
+
+	test.TestMessageAgainstGolden(t, regenerate, getProperties2,
+		filepath.Join(testData, "get_properties_2.json"),
+		test.IgnoreTimestamps{})
+
+	// Event log
+
+	polledEventLog1, err := eu.Settings.PollEventLog(authCtx, &user.PollEventLogRequest{})
+	test.Must(t, err, "poll event log")
+
+	test.TestMessageAgainstGolden(t, regenerate, polledEventLog1,
+		filepath.Join(testData, "poll_event_log_1.json"),
+		test.IgnoreTimestamps{})
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		polledEventLog2, err := eu.Settings.PollEventLog(authCtx, &user.PollEventLogRequest{AfterId: -1})
+		test.Must(t, err, "poll event log for document deletion")
+
+		test.TestMessageAgainstGolden(t, regenerate, polledEventLog2,
+			filepath.Join(testData, "poll_event_log_2.json"),
+			test.IgnoreTimestamps{})
+	}()
+
+	_, err = eu.Settings.DeleteDocument(authCtx, &user.DeleteDocumentRequest{
+		Application: docApp,
+		Type:        docType,
+		Key:         docKey,
+	})
+	test.Must(t, err, "delete document")
+
+	// Admin and shared access
+
+	adminToken := eu.AccessToken(t, elephantine.JWTClaims{
+		Scope: "user doc_admin",
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:  "test",
+			Subject: "admin-tester",
+		},
+		Units: []string{"core://unit/test"},
+		Org:   orgTest,
+	})
+
+	adminAuthCtx, _ := twirp.WithHTTPRequestHeaders(ctx, http.Header{
+		"Authorization": []string{"Bearer " + adminToken},
+	})
+
+	otherUserToken := eu.AccessToken(t, elephantine.JWTClaims{
+		Scope: "user",
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:  "test",
+			Subject: "other-user",
+		},
+		Org: "core://org/other",
+	})
+
+	otherUserAuthCtx, _ := twirp.WithHTTPRequestHeaders(ctx, http.Header{
+		"Authorization": []string{"Bearer " + otherUserToken},
+	})
+
+	_, err = eu.Settings.UpdateDocument(authCtx, &user.UpdateDocumentRequest{
+		Owner:         orgTest,
+		Application:   docApp,
+		Type:          docType,
+		Key:           docKey,
+		SchemaVersion: "v1.0.0",
+		Payload:       &newsdoc.Document{Type: docType, Title: "Org wide access"},
+	})
+	test.MustNot(t, err, "create shared document as non-admin")
+
+	_, err = eu.Settings.UpdateDocument(adminAuthCtx, &user.UpdateDocumentRequest{
+		Owner:         orgTest,
+		Application:   docApp,
+		Type:          docType,
+		Key:           docKey,
+		SchemaVersion: "v1.0.0",
+		Payload:       &newsdoc.Document{Type: docType, Title: "Org wide access"},
+	})
+	test.Must(t, err, "create shared document as admin")
+
+	_, err = eu.Settings.GetDocument(authCtx, &user.GetDocumentRequest{
+		Owner:       orgTest,
+		Application: docApp,
+		Type:        docType,
+		Key:         docKey,
+	})
+	test.Must(t, err, "get shared document as non-admin")
+
+	// List documents as non-admin should include shared doc.
+	listDocuments2, err := eu.Settings.ListDocuments(authCtx, &user.ListDocumentsRequest{
+		Application: docApp,
+	})
+	test.Must(t, err, "list documents as non-admin")
+
+	test.TestMessageAgainstGolden(t, regenerate, listDocuments2,
+		filepath.Join(testData, "list_documents_2.json"),
+		test.IgnoreTimestamps{})
+
+	_, err = eu.Settings.GetDocument(otherUserAuthCtx, &user.GetDocumentRequest{
+		Owner:       orgTest,
+		Application: docApp,
+		Type:        docType,
+		Key:         docKey,
+	})
+	test.MustNot(t, err, "get shared doc from org it doesn't belong to")
+
+	_, err = eu.Settings.UpdateDocument(adminAuthCtx, &user.UpdateDocumentRequest{
+		Owner:         "core://unit/other",
+		Application:   docApp,
+		Type:          docType,
+		Key:           docKey,
+		SchemaVersion: "v1.0.0",
+		Payload:       &newsdoc.Document{Type: docType, Title: "Unit wide access"},
+	})
+	test.MustNot(t, err, "create shared doc for unit it doesn't belong to")
+
+	// Event log for shared documents
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		polledEventLog3, err := eu.Settings.PollEventLog(authCtx, &user.PollEventLogRequest{AfterId: -1})
+		test.Must(t, err, "poll event log for shared document update")
+
+		test.TestMessageAgainstGolden(t, regenerate, polledEventLog3,
+			filepath.Join(testData, "poll_event_log_3.json"),
+			test.IgnoreTimestamps{})
+	}()
+
+	_, err = eu.Settings.UpdateDocument(adminAuthCtx, &user.UpdateDocumentRequest{
+		Owner:         orgTest,
+		Application:   docApp,
+		Type:          docType,
+		Key:           docKey,
+		SchemaVersion: "v1.0.0",
+		Payload: &newsdoc.Document{
+			Type: docType, Title: "Event Log Trigger Doc",
+		},
+	})
+	test.Must(t, err, "update shared document as admin")
+
 	wg.Wait()
 }
 
 type TestElephantUser struct {
 	JWTKey   *ecdsa.PrivateKey
 	Messages user.Messages
+	Settings user.Settings
 }
 
 func (teu *TestElephantUser) AccessToken(t *testing.T, claims elephantine.JWTClaims) string {
@@ -182,7 +421,7 @@ func (teu *TestElephantUser) AccessToken(t *testing.T, claims elephantine.JWTCla
 	return ss
 }
 
-func startElepantUser(t *testing.T) TestElephantUser {
+func startElephantUser(t *testing.T) TestElephantUser {
 	t.Helper()
 
 	ctx := t.Context()
@@ -205,6 +444,7 @@ func startElepantUser(t *testing.T) TestElephantUser {
 		ctx, logger, dbpool,
 		store.Messages,
 		store.InboxMessages,
+		store.EventLog,
 	)
 
 	validator, err := internal.NewValidator(ctx)
@@ -234,10 +474,12 @@ func startElepantUser(t *testing.T) TestElephantUser {
 	test.Must(t, err, "run application")
 
 	messages := user.NewMessagesProtobufClient("http://"+apiServer.Addr(), client)
+	settings := user.NewSettingsProtobufClient("http://"+apiServer.Addr(), client)
 
 	return TestElephantUser{
 		JWTKey:   jwtKey,
 		Messages: messages,
+		Settings: settings,
 	}
 }
 
